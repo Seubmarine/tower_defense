fd35ae19ece1c1b17efd9b9f7c4b78c623cf425e
Seubmarine <Seubmarinepro@gmail.com>
2021-03-28 00:46:13 +0100

    Unit move on tilemap

--- const.py
+++ const.py
@@ -1,1 +1,12 @@
-TILEMAP_DATA = {1 : "up", 2 : "right", 3 : "down", 4 : "left"}
+TILEMAP_DATA = {
+    1 : "up",
+    2 : "right",
+    3 : "down",
+    4 : "left",
+    5 : "up_down",
+    6 : "left_right",
+    7 : "down_left",
+    8 : "down_right",
+    9 : "up_left",
+    10 : "up_right"
+    }
--- entity.py
+++ entity.py
@@ -1,18 +1,25 @@
 import pyxel
+import random
 from vector import Vector2
 from const import TILEMAP_DATA
 
 class Entity:
     def __init__(self, x=0, y=0):
         self.position = Vector2(x, y)
+        # self.speed = random.uniform(0.1, 0.9)
         self.velocity = Vector2.ZERO
         self.direction = "right"
 
     def get_timemap_pos(self):
-        return pyxel.tilemap(0).get(self.position.x, self.position.y)
+        return pyxel.tilemap(0).get(self.position.x / 8, self.position.y / 8)
     
-    def update(self):
-        self.direction = TILEMAP_DATA.get(self.get_timemap_pos())
+    def random_direction(self, choice1, choise2):
+        if bool(random.getrandbits(1)):
+                self.velocity = choice1
+        else:
+            self.velocity = choise2
+
+    def change_velocity(self):
         if self.direction == "up":
             self.velocity = Vector2.UP
         elif self.direction == "left":
@@ -21,11 +28,26 @@ class Entity:
             self.velocity = Vector2.DOWN
         elif self.direction == "right":
             self.velocity = Vector2.RIGHT
+        elif self.direction == "up_down":
+            self.random_direction(Vector2.UP, Vector2.DOWN)
+        elif self.direction == "left_right":
+            self.random_direction(Vector2.RIGHT, Vector2.LEFT)
+        elif self.direction == "down_left":
+            self.random_direction(Vector2.DOWN, Vector2.LEFT)
+        elif self.direction == "down_right":
+            self.random_direction(Vector2.DOWN, Vector2.RIGHT)
+        elif self.direction == "up_left":
+            self.random_direction(Vector2.UP, Vector2.LEFT)
+        elif self.direction == "up_right":
+            self.random_direction(Vector2.UP, Vector2.RIGHT)
         else:
-            self.velocity = Vector2.RIGHT
-        self.position += self.velocity
+            self.velocity = Vector2.ZERO
 
-        
+    def update(self):
+        if self.position.x % 8 == 0 and self.position.y % 8 == 0:
+            self.direction = TILEMAP_DATA.get(self.get_timemap_pos())
+            self.change_velocity()
+        self.position += self.velocity #* self.speed
 
     def draw(self):
-        pyxel.rectb(self.position.x * 8, self.position.y * 8, 8, 8, 7)
+        pyxel.rectb(self.position.x, self.position.y, 8, 8, 7)
