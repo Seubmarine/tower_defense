cec7aad1aa64afb4599cf80d2cdd84c4aef1ccfc
Seubmarine <Seubmarinepro@gmail.com>
2021-03-29 04:34:39 +0200

    better entity collision

--- entity.py
+++ entity.py
@@ -4,14 +4,48 @@ from vector import Vector2
 from const import TILEMAP_DATA
 
 class Entity:
+    
     def __init__(self, x=0, y=0):
-        self.position = Vector2(x, y)
-        # self.speed = random.uniform(0.1, 0.9)
+        self.set_coordinate(x, y)
         self.velocity = Vector2.ZERO
-        self.direction = "right"
+
+    def set_coordinate(self, x, y):
+        self.position = Vector2(x, y)
+        self.cell_pos = self.position / 8
+        self.ratio_pos = (self.position - self.cell_pos*8) / 8
+
+    def update(self):
+        while self.ratio_pos.x > 1:
+            self.cell_pos.x += 1
+            self.ratio_pos.x -= 1
+        
+        while self.ratio_pos.x < 0:
+            self.cell_pos.x -= 1
+            self.ratio_pos.x += 1
+
+        while self.ratio_pos.y > 1:
+            self.cell_pos.y += 1
+            self.ratio_pos.y -= 1
+        
+        while self.ratio_pos.y < 0:
+            self.cell_pos.y -= 1
+            self.ratio_pos.y += 1
+
+        self.position = (self.cell_pos + self.ratio_pos) * 8
+            
+    def draw(self):
+        pyxel.rectb(self.position.x, self.position.y, 8, 8, pyxel.COLOR_WHITE)
+
+class Enemy(Entity):
+    def __init__(self, x=0, y=0):
+        super().__init__(x, y)
+        self.last_cell_pos = Vector2(-1,-1)
+        self.speed = random.uniform(0.05, 0.1)
+        self.velocity = Vector2.ZERO
+        self.direction = "down"
 
     def get_timemap_pos(self):
-        return pyxel.tilemap(0).get(self.position.x / 8, self.position.y / 8)
+        return pyxel.tilemap(0).get(self.cell_pos.x, self.cell_pos.y)
     
     def random_direction(self, choice1, choise2):
         if bool(random.getrandbits(1)):
@@ -44,10 +78,15 @@ class Entity:
             self.velocity = Vector2.ZERO
 
     def update(self):
-        if self.position.x % 8 == 0 and self.position.y % 8 == 0:
+        # print("cell: {}, l_cell {}, same : {}".format( self.cell_pos, self.last_cell_pos, self.last_cell_pos == self.cell_pos))
+        # print(self.ratio_pos, (self.ratio_pos <= Vector2(0.1, 0.1)) == (True, True))
+        if self.cell_pos != self.last_cell_pos and ((self.ratio_pos <= Vector2(0.1, 0.1)) == (True, True)):
             self.direction = TILEMAP_DATA.get(self.get_timemap_pos())
             self.change_velocity()
-        self.position += self.velocity #* self.speed
+            self.last_cell_pos = Vector2(self.cell_pos.x, self.cell_pos.y) # idk why i can't use self.last_cell_pos = self.cell_pos for some reason
+        self.ratio_pos += self.velocity * self.speed
+        
+        super().update()
 
     def draw(self):
         pyxel.rectb(self.position.x, self.position.y, 8, 8, 7)
